abfb0a19e44fa0718ac21eeb172dd233b2099314
skazko <kreeon@ya.ru>
2026-02-07 15:41:19 +0000

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://curious-douhua-b9bee1.netlify.app"
+});
